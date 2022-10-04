bc999dbb1cdd58b05e92de25974fac6506f86ac2
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-10-05 02:32:10 +0530

    first commit

---
+++ 1_addNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int amount1, amount2;
+    cout << "enter the amount1" << endl;
+    cin >> amount1;
+    cout << "Enter the amount2" << endl;
+    cin >> amount2;
+    int sum = amount1 + amount2;
+    cout << "The sum of " <<amount1<<"and " << amount2<<"is "<<sum<<endl;
+    
+    return 0;  
+}
---
+++ fristprogram.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"hello abhishek"<<endl;
+return 0;
+}
+
+
