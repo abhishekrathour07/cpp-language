76fcd0ca38bb87d485f0a937117e41a4e9e2f705
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-10-10 22:32:39 +0530

    new files

---
+++ 3_2_forLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int i,num;
+    cout<<"Enter the num"<<endl;
+    cin>>num;
+    for(i=1;i<11;i++){
+       int table =num*i;
+       cout <<"muntiplication table of "<<num<<" is "<<table<<endl;
+
+    }
+return 0;
+}
---
+++ 3_3whileLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=0,num,table;
+    cout<<"Enter the number to find table"<<endl;
+    cin>>num;
+    while(i<10){
+        i++;
+        table = num *i;
+    cout << num<<"x "<<i<<" is "<<table<<endl;
+    }
+    return 0;
+}
