051d54e97eb63232a3a5c1468c0763eb3af81cf0
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-10-21 02:30:56 +0530

    constructor>

---
+++ 9constructorOverloading.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class complex {
+    int a,b;
+    public:
+    complex(int x,int y){
+        a=x;
+        b=y;
+    }
+        complex(int x){
+            a=x;
+            b=0;
+        }
+};
+
+int main(){
+ 
+
+ return 0;
+}
