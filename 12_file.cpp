4801afb4a76ddc666a699539062d788c2c5df86f
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2022-10-31 23:35:28 +0530

    file i/o

---
+++ 12_file.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+string st1 ="Hello abhishek singh rathour ";
+string st2;
+ofstream likhde("sample12a.txt");
+likhde<<st1;
+ifstream dikhade("sample12b.txt");
+dikhade>>st2;
+cout<<st2;
+getline(dikhade,st2);
+cout<<st2;
+
+    return 0;
+}
